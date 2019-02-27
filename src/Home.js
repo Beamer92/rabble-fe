447f02bad9c27811072cac08fde06a4e42892180
Brendan Woodell <brendan.woodell@gmail.com>
2019-02-27 12:33:15 -0800

    added credits for icon

--- src/Home.js
+++ src/Home.js
@@ -408,6 +408,9 @@ class Home extends Component {
                         <Button className='rabble' color={this.state.myTurn ? 'success' : 'secondary'} onClick={this.submitRabble} disabled={!this.state.myTurn ? true : false}>Submit Rabble</Button>
                     </div>
                 </div>
+                <div className='credits'>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 
+      title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"
+      title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             </div>
         )
     }
